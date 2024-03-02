cf00d23d8d767cfbefb18075a7f881fdefb15dab
Mel <melrosalinda04@gmail.com>
2024-03-02 18:47:55 +0100

    canvi 2 mel

--- code_1.py
+++ code_1.py
@@ -7,20 +7,24 @@ def main():
     while True: # Infinite loop to show menu and receive options from user
 
         print("1. Add contact") # Option to add a new contact
-        print("2. View contacts")
-        print("3. Search contact")
-        print("4. Exit")
+        print("2. View contacts") # Option to view contact list
+        print("3. Search contact") # Option to search for a contact
+        print("4. Exit") # Option to exit the program
         
-        opcion = input("Select an option (1/2/3/4): ")
+        opcion = input("Select an option (1/2/3/4): ") # We ask the user to select an option
         
-        if opcion == "1":
+        if opcion == "1":   # If the user chooses option 1 
+                            #we call the agregar_contactos function to add a new contact
             agregar_contacto(agenda)
-        elif opcion == "2":
+
+        elif opcion == "2": # If the user chooses option 2
+                            # We call the ver_contactos function to display the contact list
             ver_contactos(agenda)
-        elif opcion == "3":
+        elif opcion == "3": # If the user chooses option 3
+                            # We call the buscar_contacto function to search for a contact
             buscar_contacto(agenda)
-        elif opcion == "4":
-            print("¡See you soon!")
-            break
-        else:
-            print("Invalid option. Please select a valid option.")
+        elif opcion == "4": # If the user chooses option 4
+            print("¡See you soon!") # Goodbye message
+            break   # We exit the loop, ending the program
+        else:   #If the user enters an invalid option
+            print("Invalid option. Please select a valid option.") # Invalid option message
